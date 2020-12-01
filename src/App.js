726dc50f65c890d89b4055d87d89d3d40a500318
Chris <chris@ChristophersMBP.fios-router.home>
2020-12-01 00:29:00 -0500

    ui

--- src/App.js
+++ src/App.js
@@ -4,32 +4,39 @@ import Login from './Login';
 import { getTokenFromResponse } from './spotify'
 import SpotifyWebApi from 'spotify-web-api-js';
 import Player from "./Player"
+import { useDataLayerValue } from "./DataLayer";
 
 const spotify = new SpotifyWebApi();
 
 function App() {
-  const [token, setToken] = useState(null);
-
+  const [{ user, token }, dispatch] = useDataLayerValue(); 
 
   useEffect(() => {
     const hash = getTokenFromResponse()
-    window.location.hash = " ";
+    window.location.hash = "";
     const _token = hash.access_token;
 
     if (_token) {
-      setToken(_token)
+
+      dispatch({
+        type: "SET_TOKEN",
+        token: _token,
+      })
 
       spotify.setAccessToken(_token)
 
       spotify.getMe().then(user => {
-        console.log(user);
-      })
+        dispatch({
+          type: 'SET_USER',
+          user: user
+        });
+      });
     }
 
-  }, [])
+  }, []);
 
   return <div className="app"> {token ? 
-    <Player /> : 
+    <Player spotify = {spotify} /> : 
     <Login />} </div>
 }
 
--- src/Player.js
+++ src/Player.js
@@ -1,11 +1,19 @@
 import React from 'react'
+import "./Player.css";
+import Sidebar from "./Sidebar"
+import Body from "./Body"
+import Footer from "./Footer"
 
-function Player() {
+function Player( spotify ) {
     return (
-        <div>
-            <h1>Eat my Taint</h1>
+        <div className = "player_body">
+            <div className = "player_body">
+                <Sidebar />
+                <Body />
+            </div>
+            <Footer />
         </div>
-    )
+    );
 }
 
 export default Player
